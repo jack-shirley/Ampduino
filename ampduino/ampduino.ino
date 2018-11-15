842090e60d8b3baaadf4d6cb82df274f6fffa94a
Andrew Schwenn <aschwenn@verizon.net>
2018-11-15 12:57:31 -0600

    loopback - reads A0 and immediately outputs to A11 pwm

--- ampduino/ampduino.ino
+++ ampduino/ampduino.ino
@@ -3,19 +3,19 @@
 /* GLOBALS */
 #define onLED 12
 // input = A0
-// output = 9, 10
+// output = 11
 
 /* ADC */
-unsigned int adcLOW, adcHIGH;
-int adcINPUT;
+// "volatile" must be used for variables inside interrupt handlers
+volatile boolean sample;
+volatile byte adc0;
 
-/* PWM */
-#define freq 0x00FF // 31.3kHz
-#define mode 0      // phase correct mode
-#define qty 2       // number of PWMs in parallel (1-3)
+/* AVR Commands */
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit)) // clear bit
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))  // set bit
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
   
   // Pins
   pinMode(onLED, OUTPUT);
@@ -33,44 +33,96 @@ void setup() {
   // ADIF = ADC interrupt flag
   // ADIE = ADC interrupt enable
   // ADPS = prescaler bits, clock rate/ADPS = ADC clock freq
-  ADCSRA = B11100101; // turn on ADC, clockrate/32
+  sbi(ADCSRA, ADEN); // enable ADC
+  //sbi(ADCSRA, ADSC); // start
+  //sbi(ADCSRA, ADFR); // free running mode
+  // set prescaler to 64 for clock/64 = 19kHz sampling freq
+  cbi(ADCSRA, ADPS2);
+  sbi(ADCSRA, ADPS1);
+  sbi(ADCSRA, ADPS0);
 
   // ADMUX Bits: REFS1,REFS0,ADLAR,x,MUX3,MUX2,MUX1,MUX0
   // REFS = selector for internal reference voltage
   // ADLAR = ADC left adjust result
-  // x = not used
   // MUX = analog channel selection bits, ADC0 = 0000
-  ADMUX = B1100000;
+  sbi(ADMUX, ADLAR); // left adjust, 8-bit ADC will be available for use in ADCH register
+  // use internal VCC reference
+  sbi(ADMUX, REFS0);
+  cbi(ADMUX, REFS1);
+  // use ADC0 or pin A0 to read from
+  cbi(ADMUX, MUX0);
+  cbi(ADMUX, MUX1);
+  cbi(ADMUX, MUX2);
+  cbi(ADMUX, MUX3);
 
-  // ADCSRB
-  ADCSRB = B0000111;
-
-  // DIDR0
-  DIDR0 = B0000001; // turn off digital inputs for ADC0
-
-  /* PWM Setup */
-  // Initialize Timer1
-  TCCR1A = 0;
-  TCCR1B = 0;
-  // TCCR1A: Timer/Counter Control Register 1A - setting Timer1
-  TCCR1A = (((qty - 1) << 5) | 0x80 | (mode << 1));
-  // TCCR1B: Timer/Counter Control Register 1B
-  // Last 3 bits determine the timer clock setting
-  TCCR1B = ((mode << 3) | 0x11); // no clock prescaling
-  // more timers
-  ICR1L = (freq & 0xff);
-  DDRB |= ((qty << 1) | 0x02);
-  sei(); // enable interrupts
+  /* Timer Setup - Using Timer 2 */
+  // initialize timer2
+  noInterrupts(); // disable interrupts while configuring
+  TCCR2A = 0;
+  TCCR2B = 0;
+  // TCCR2A Bits: COM2A1,COM2A0,COM2B1,COM2B0,x,x,WGM21,WGM20
+  // TCCR2B Bits: ICNC2,ICES2,x,WGM23,WGM22,CS22,CS21,CS20
+  /* Prescaling (CS2x bits)
+   *  000 - no clock source (timer stopped)
+   *  001 - clk/1 (no prescaling)
+   *  010 - clk/8
+   *  011 - clk/64
+   *  100 - clk/256
+   *  101 - clk/1024
+   *  110,111 - external clock source (falling edge, rising edge)
+   * Timer Modes (WGM2x bits)
+   *  0000 - normal
+   *  0001 - PWM, phase correct, 8-bit
+   *  0010 - PWM, phase correct, 9-bit
+   *  0011 - PWM, phase correct, 10-bit
+   *  0100 - CTC
+   *  0101 - Fast PWM, 8-bit
+   *  0110 - Fast PWM, 9-bit
+   *  0111 - Fast PWM, 10-bit
+   *  1000 - PWM, phase and frequency correct
+   *  1001 - PWM, phase and frequency correct
+   *  1010 - PWM, phase correct
+   *  1011 - PWM, phase correct
+   *  1100 - CTC
+   *  1110 - Fast PWM
+   *  1111 - Fast PWM
+   */
+  cbi(TCCR2A, COM2A0);
+  sbi(TCCR2A, COM2A1);
+  // set to PWM, phase correct, 9-bit
+  sbi(TCCR2A, WGM20);
+  sbi(TCCR2A, WGM21);
+  cbi(TCCR2B, WGM22);
+  //cbi(TCCR2B, WGM23);
+  // set timer2 prescaler to 1
+  sbi(TCCR2B, CS20);
+  cbi(TCCR2B, CS21);
+  cbi(TCCR2B, CS22);
+  // enable PWM port
+  sbi(DDRB, 3);
+  interrupts(); // re-enable interrupts
+  cbi(TIMSK0, TOIE0); // turned off timer0 --- DELAY() is off now
+  sbi(TIMSK2, TOIE2); // enable timer2 interrupt
 }
 
 void loop() {
-  if(true){
+  /*if(false){
     if(analogRead(0) > 512) analogWrite(9,255);
     else analogWrite(9,0);
   }
   else{
     analogWrite(9,analogRead(0)/4);
-  }
+  }*/
+
+  while(!sample){ } // wait until sample is received
+  OCR2A = adc0; // output audio to PWM port A11
+  sample = false; // reset sample
+}
+
+ISR(TIMER2_OVF_vect){
+  adc0 = ADCH;
+  sample = true; // alert the loop that there is a sample available
+  sbi(ADCSRA, ADSC); // start conversion again
 }
 
 
